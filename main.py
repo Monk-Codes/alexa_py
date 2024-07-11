72a338d49b3e8d1c8d0377f729750cd06901e234
monk-codes <your_donotreply_email_id>
2024-07-11 14:31:05 +0530

    voice assist

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr
 import webbrowser
 import pyttsx3
+import musicLibrary
 # Imports
 
 # Initialization
@@ -13,11 +14,37 @@ def speak(text):
   engine.runAndWait()
 
 def processCommand(c):
-   print(c)
-   pass
+   if "open google" in c.lower():
+      speak("opening google...")
+      webbrowser.open("https://google.com")
+   elif "open stackoverflow" in c.lower():
+      speak("opening stack overflow...")
+      webbrowser.open("https://stackoverflow.com")
+   elif "open youtube" in c.lower():
+      speak("opening youtube...")
+      webbrowser.open("https://www.youtube.com")
+   elif "open instagram" in c.lower():
+      speak("opening instagram...")
+      webbrowser.open("https://www.instagram.com")
+   elif "open twitter" in c.lower():
+      speak("opening twitter...")
+      webbrowser.open("https://twitter.com")
+   elif "open facebook" in c.lower():
+      speak("opening facebook...")
+      webbrowser.open("https://www.facebook.com")
+   elif "open linkedin" in c.lower():
+      speak("opening linkedin...")
+      webbrowser.open("https://www.linkedin.com")
+   elif "open whatsapp" in c.lower():
+      speak("opening whatsapp...")
+      webbrowser.open("https://www.web.whatsapp.com")
+   elif c.lower().startswith("play"):
+      song=c.lower().split(" ")[1]
+      link=musicLibrary.music[song]
+      webbrowser.open(link)
 
 if __name__=="__main__":
-  speak("Initializing Alexa...")
+  speak("Initializing luna...")
   while True:
     r=sr.Recognizer()
 
@@ -25,13 +52,13 @@ if __name__=="__main__":
     try:
         with sr.Microphone() as source:
             print("Listening...")
-            audio=r.listen(source,timeout=2,phrase_time_limit=1)
+            audio=r.listen(source,timeout=1,phrase_time_limit=1)
         word=r.recognize_google(audio)
         print("You said: ",word)
-        if(word.lower()=="alexa"):
+        if(word.lower()=="luna"):
           speak('yesss listening')
           with sr.Microphone() as source:
-             print("alexa listening")
+             print("luna listening")
              audio=r.listen(source)
              command=r.recognize_google(audio)
              processCommand(command)
---
+++ musicLibrary.py
@@ -0,0 +1,5 @@
+music ={
+  "fun":"https://www.youtube.com/watch?v=tOM-nWPcR4U&pp=ygUPaWxsdW1pbmF0aSBzb25n",
+  "skyfall":"https://www.youtube.com/watch?v=LK7-_dgAVQE&pp=ygULdGF1YmEgdGF1YmE%3D",
+  "naadaniya":"https://www.youtube.com/watch?v=gPpQNzQP6gE&ab_channel=AkshathAcharya"
+}
